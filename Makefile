149dc037ce867a5eeb160baa7ab859dbe328df0b
MarkelUribe <uribemarkel@gmail.com>
2024-01-04 00:22:11 +0100

    obligatoyuek inda

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/13 15:15:27 by muribe-l          #+#    #+#              #
-#    Updated: 2024/01/03 20:54:26 by muribe-l         ###   ########.fr        #
+#    Updated: 2024/01/03 22:33:40 by muribe-l         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,8 @@ SOURCES = ft_calloc.c ft_isalpha.c ft_memchr.c ft_memset.c ft_strlen.c ft_tolowe
 ft_isascii.c ft_memcmp.c ft_strchr.c ft_strncmp.c ft_toupper.c ft_bzero.c \
 ft_isdigit.c ft_memcpy.c ft_strlcat.c ft_strnstr.c ft_isalnum.c ft_isprint.c \
 ft_memmove.c ft_strlcpy.c ft_strrchr.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c \
-ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c
+ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c \
+ft_putendl_fd.c ft_putnbr_fd.c
 
 OBJECTS = $(SOURCES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,13 +6,21 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:15:24 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/03 20:54:09 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/03 23:00:48 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t num, size_t size);
@@ -44,3 +52,7 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
