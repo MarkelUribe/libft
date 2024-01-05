1fd8e7a3cbee1479f42e660b4d8da96f69f71f37
MarkelUribe <uribemarkel@gmail.com>
2024-01-05 14:58:36 +0100

    lstmap turbio ziok

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: muribe-l <muribe-l@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 17:17:47 by muribe-l          #+#    #+#             */
-/*   Updated: 2024/01/04 17:20:51 by muribe-l         ###   ########.fr       */
+/*   Updated: 2024/01/05 14:58:22 by muribe-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new;
+	t_list	*tmp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new = malloc(sizeof(t_list) * ft_lstsize(lst));
+	if (!new)
+		return (NULL);
+	while (lst)
+	{
+		tmp = ft_lstnew(lst->content);
+		if (!tmp)
+		{
+			
+		}
+	}
+	return (new);
 }
